749a0c1428b1d53d919653ce09251faf836eac0a
Sergei Maertens <sergei@maykinmedia.nl>
2016-04-01 11:05:43 +0200

    Remove some testing cruft

--- tests/relations.spec.js
+++ tests/relations.spec.js
@@ -11,16 +11,6 @@ let Kit = Model('Kit', {
 });
 
 
-
-let generateResponse = function(object, status=200) {
-    return [
-        status,
-        {'Content-type': 'application/json'},
-        JSON.stringify(object)
-    ];
-}
-
-
 describe('The base relation descriptor', () => {
 
     it('should throw on getter/setter access', () => {
@@ -33,25 +23,6 @@ describe('The base relation descriptor', () => {
 
 
 describe('Models with relation fields', () => {
-
-    let server = null;
-
-    beforeEach(() => {
-        server = sinon.fakeServer.create();
-        server.autoRespond = true;
-        server.xhr.useFilters = true;
-
-        server.xhr.addFilter(function (method, uri) {
-            let matched = uri.startsWith('http://example.com') || uri.startsWith('http://api.external.com');
-            // Sinon FakeXHR filters need to return `false` if the request should be stubbed and
-            // `true` if it should be allowed to pass through.
-            return !matched;
-        });
-    });
-
-    afterEach(() => {
-        server.restore();
-    });
 
     it('should return null if the nested data is not present', () => {
         let kit = new Kit({id: 1});
@@ -74,36 +45,4 @@ describe('Models with relation fields', () => {
         expect(kit.brand).to.deep.equal({id: 2});
     });
 
-    it('should resolve nested foreign keys', () => {
-        let rawKit = {
-            "id": 1,
-            "name": "S100 & Flak 38 Schnellboot",
-            "kit_number": "",
-            "box_image": {
-                "small": null
-            },
-            "brand": {
-                "id": 2,
-                "name": "Revell",
-                "is_active": true,
-                "logo": {
-                    "small": "http://example.com/media/image.jpg"
-                }
-            }
-        };
-        var okResponse = generateResponse(rawKit);
-        server.respondWith('GET', 'http://example.com/api/v1/kit/1/', okResponse);
-        let expectedBrand = new Brand({"id": 2,
-            "name": "Revell",
-            "is_active": true,
-            "logo": {
-                "small": "http://example.com/media/image.jpg"
-            }
-        });
-        return Kit.objects.get({id: 1}).should
-            .eventually.be.an.instanceof(Kit)
-            .and.satisfy(kit => {
-                return (kit.brand instanceof Brand);
-            });
-    });
 });
